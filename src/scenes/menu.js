b8e482becdad89d62659b91fd99ab96bec78bbd5
Neffer2 <n3f73r@gmail.com>
2024-09-12 11:24:36 -0500

    1. Arepa por defecto

--- src/scenes/menu.js
+++ src/scenes/menu.js
@@ -1,4 +1,4 @@
-let arepaHuevo, arepaCarne, arepaMq, startBtn, mContext, tipoArepa;
+let arepaHuevo, arepaCarne, arepaMq, startBtn, mContext, tipoArepa = 'arepa-huevo';
 
 export class Menu extends Phaser.Scene {
     constructor ()
